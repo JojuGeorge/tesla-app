687088e8eb0166a775273e6082f84f8d8d61c669
JojuGeorge <jojugeorge001@gmail.com>
2025-01-09 15:51:21 +0530

    dependencies installed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Tesla</div>;
+  return (
+    <div className="App">
+      <h1 className="text-2xl font-bold underline">Hello world!</h1>
+      <button class="btn btn-primary">Primary</button>
+    </div>
+  );
 }
 
 export default App;
