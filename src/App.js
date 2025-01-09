48c009d3cd4754ae1796051288f83205b187860c
JojuGeorge <jojugeorge001@gmail.com>
2025-01-09 16:47:42 +0530

    basic pages, react-routing, navigation,  navbar component in tailwind Daisy UI done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import ProductList from "./pages/ProductList";
+import CarDetails from "./pages/CarDetails";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-2xl font-bold underline">Hello world!</h1>
-      <button class="btn btn-primary">Primary</button>
+      <NavBar />
+
+      <Routes>
+        <Route path="/" index element={<LandingPage />} />
+        <Route path="ProductList" element={<ProductList />} />
+        <Route path="CarDetails" element={<CarDetails />} />
+      </Routes>
     </div>
   );
 }
